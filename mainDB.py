3a029c1ae74c46faa11f33b2297ec172a49259ac
a1exluvubro <bam227@mail.ru>
2022-07-07 21:19:59 +0300

    Update mainDB.py

--- mainDB.py
+++ mainDB.py
@@ -9,7 +9,7 @@ class indexes:
     node = dict()
 
 class edges: #edges = ребра
-    def __init__(self, edge_type = "sea", edge_id = 0, ice_condition = 1, length = 1, incident_nodes = "*id_begin_node*_*id_end_node*", max_throughput = 1, tariff = 1500):
+    def __init__(self, edge_type = "sea", edge_id = None, ice_condition = 1, length = 1, incident_nodes = "*id_begin_node*_*id_end_node*", max_throughput = 1, tariff = 1500):
         self.edge_type = edge_type
         self.edge_id = edge_id
         self.ice_condition = ice_condition
@@ -25,7 +25,7 @@ class edges: #edges = ребра
         #обновление поля из таблицы
 
 class ship: #ship = корабль
-    def __init__(self, ship_id = -1, edge_position = 1,edge_id = -1, port_id = 1, in_port = True, icebreaker_id = 1, max_capacity = 1, node_id = 1, cargo_type = "", caravan_condition = True):
+    def __init__(self, ship_id = None, edge_position = 1,edge_id = -1, port_id = 1, in_port = True, icebreaker_id = 1, max_capacity = 1, node_id = 1, cargo_type = "", caravan_condition = True):
         self.ship_id = ship_id
         self.edge_position = edge_position
         self.edge_id = edge_id
@@ -39,7 +39,7 @@ class ship: #ship = корабль
         self.caravan_condition = caravan_condition
 
 class consignment: #consignment = партия груза
-    def __init__(self, cargo_id = -1, size = 1, node_destination_id = 1, ship_immediately = True, type_refer = 1, id_refer = 1, contracted = True):
+    def __init__(self, cargo_id = None, size = 1, node_destination_id = 1, ship_immediately = True, type_refer = 1, id_refer = 1, contracted = True):
         self.cargo_id = cargo_id
         self.size = size
         self.node_destination_id = node_destination_id
@@ -51,10 +51,10 @@ class consignment: #consignment = партия груза
     def create(self):
         with sq.connect("Ships_Icebreakers.db") as con:
             cur = con.cursor()
-            cur.execute("insert into consignment ()"
+            cur.execute("insert into consignment (cargo_id, size, node_destination_id, ship_immediately, type_refer, id_refer, coordinates, contracted) values ('{cargo_id}', '{size}', '{node_destination_id}', '{ship_immediately}', '{type_refer}', '{id_refer}', '{coordinates}', '{contracted}')"
 
 class icebreaker: #icebreaker = ледокол
-    def __init__(self, icebreaker_id = 0, edge_position = 0, prepare_caravan = True, edge_id = 0, port_id = 0, node_destination_id = 1, speed = 40,shipsin_caravan = True):
+    def __init__(self, icebreaker_id = None, edge_position = 0, prepare_caravan = True, edge_id = 0, port_id = 0, node_destination_id = 1, speed = 40,shipsin_caravan = True):
         self.icebreaker_id = icebreaker_id
         self.edge_position = edge_position
         self.prepare_caravan = prepare_caravan
@@ -69,7 +69,7 @@ class icebreaker: #icebreaker = ледокол
             cur.execute("insert into icebreaker (icebreaker_id, edge_position, prepare_caravan, edge_id, port_id, node_destination_id, speed, shipsin_caravan) values ('{icebreaker_id}', '{edge_position}', '{prepare_caravan}', '{edge_id}', '{node_destination_id}', '{speed}', '{shipsin_caravan}')"
 
 class node: #node = узел
-    def __init__(self, node_id = 0):
+    def __init__(self, node_id = None):
         self.coordinates = coordinates
         self.node_id = node_id
     def create(self):
